3b8396dba6fc41d6607aa6d736864453f3e775d8
panosgeorgakopoulos <pangegeorg@gmail.com>
2026-06-29 16:10:12 +0300

    fix: update vite config for missing inputs

--- vite.config.js
+++ vite.config.js
@@ -8,15 +8,13 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         field: resolve(__dirname, 'field.html'),
+        strategy: resolve(__dirname, 'strategy.html'),
+        profile: resolve(__dirname, 'profile.html'),
         about: resolve(__dirname, 'about.html'),
         faq: resolve(__dirname, 'faq.html'),
         guide: resolve(__dirname, 'guide.html'),
         detailed_prediction: resolve(__dirname, 'detailed_prediction.html'),
         weights: resolve(__dirname, 'weights.html'),
-        field_1: resolve(__dirname, 'field_1.html'),
-        field_2: resolve(__dirname, 'field_2.html'),
-        field_3: resolve(__dirname, 'field_3.html'),
-        field_4: resolve(__dirname, 'field_4.html'),
       }
     }
   }
